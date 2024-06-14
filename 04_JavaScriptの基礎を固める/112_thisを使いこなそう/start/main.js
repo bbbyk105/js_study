2e6817d1f4faf32c4ed4b40eede23b34a063f6bf
近藤白虎 <bbbyk105@yahoo.co.jp>
2024-06-14 17:38:56 +0900

    how to use 'this' on JS

--- 04_JavaScriptの基礎を固める/112_thisを使いこなそう/start/main.js
+++ 04_JavaScriptの基礎を固める/112_thisを使いこなそう/start/main.js
@@ -1,12 +1,16 @@
 document.addEventListener('DOMContentLoaded', function () {
+    const btn = document.querySelector('#btn');
     const ta = new TextAnimation('.animate-title');
     const ta2 = new TextAnimation('.animate-title-2');
     ta.animate();
     ta2.animate();
+    btn.addEventListener("click", ta.animate.bind(ta))
 });
+
 
 class TextAnimation {
     constructor(el) {
+        console.log(this);
         this.el = document.querySelector(el);
         this.chars = this.el.innerHTML.trim().split("");
         this.el.innerHTML = this._splitText();
@@ -18,6 +22,7 @@ class TextAnimation {
         }, "");
     }
     animate() {
+        console.log(this);
         this.el.classList.toggle('inview');
     }
 }
