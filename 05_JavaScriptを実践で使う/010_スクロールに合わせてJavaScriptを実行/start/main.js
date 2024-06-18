734c3d578d35fe3c5cfcd78fbb7a13856686f75a
近藤白虎 <bbbyk105@yahoo.co.jp>
2024-06-18 17:15:25 +0900

    intersectionObserver

---
+++ 05_JavaScriptを実践で使う/010_スクロールに合わせてJavaScriptを実行/start/main.js
@@ -0,0 +1,22 @@
+const child = document.querySelector(".child");
+const cb = function(entries, observer) {
+    entries.forEach(entry => {
+        if(entry.isIntersecting){
+            console.log("inview");
+            entry.target.classList.add('inview');
+            // observer.unobserve(entry.target);
+
+        }else {
+            entry.target.classList.remove('inview');
+        }
+    });
+    // alert('intersecting');
+}
+const options = {
+    root: null,
+    rootMargin: "0px";
+}
+const io = new IntersectionObserver(cb, options);
+io.observe(child);
+// io.observe(child1);
+// io.observe(child2);
