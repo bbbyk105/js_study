9ff98c7fbba596d593f3a08d0a916c7ae4b0dbc8
近藤白虎 <bbbyk105@yahoo.co.jp>
2024-06-28 23:02:11 +0900

    insert-pictures

--- 98_サンプルサイト_練習用/scripts/main.js
+++ 98_サンプルサイト_練習用/scripts/main.js
@@ -1,4 +1,24 @@
 document.addEventListener('DOMContentLoaded', function () {
     const hero = new HeroSlider('.swiper');
     hero.start();
+
+    const _textAnimation = function (el, inview) {
+        if(inview) {
+            const ta = new TweenTextAnimation(el);
+            ta.animate();
+        }
+    }
+
+    const so = new ScrollObserver('.tween-animate-title', _textAnimation);
+
+    const _inviewAnimation = function (el, isIntersecting) {
+        if(isIntersecting) {
+            el.classList.add("inview")
+        }else{
+            el.classList.remove("inview")
+        }
+    }
+
+    const so2 = new ScrollObserver('.cover-slide', _inviewAnimation);
 });
+
